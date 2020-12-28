9335097c346717ee2e2fcaa733c766cd2693fa29
Maxence <lemairemaxence@hotmail.com>
2020-12-28 16:57:06 +0100

    exo 5 ok

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+// EXO 1
+
+// let chiffre1 = 1
+// let chiffre2 = 2
+// let addition = chiffre1 + chiffre2
+// let soustraction = chiffre1 - chiffre2
+// let division = chiffre1 / chiffre2
+// let multiplication = chiffre1 * chiffre2
+
+// alert(addition);
+// alert(soustraction);
+// alert(division);
+// alert(multiplication);
+
+// EXO 2
+
+let age = prompt("Ton âge ?")
+let prenom = prompt("Ton prénom ?")
+age = parseInt(age)
+
+console.log(`${prenom} a ${age} ans cette année`);
+console.log(`${prenom} aura ${age+3} ans dans 3 ans`);
+
